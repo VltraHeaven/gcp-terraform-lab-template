2f5e5a25ebc099a93789bd4d2a57db45975223c9
Julio H <julio@vltraheaven.io>
2020-10-21 06:45:57 -0700

    Added main.tf with base provider configuration

---
+++ main.tf
@@ -0,0 +1,27 @@
+# Terraform Config Details
+terraform {
+  required_version = ">=0.13.2"
+}
+
+# Provider info with project name, region and zone
+provider "google" {
+  credentials   = var.credkey
+  project       = var.project
+  region        = var.region
+}
+
+# Network configuration for the project
+resource "google_compute_network" "vpc_network" {
+  name = var.net
+  auto_create_subnetworks = true
+  project = var.project
+}
+
+# Sets the metadata for the project to enable OS Login
+
+resource "google_compute_project_metadata_item" "os-login" {
+  key           = "enable-oslogin"
+  value         = var.enable_oslogin
+  project       = var.project
+}
+
